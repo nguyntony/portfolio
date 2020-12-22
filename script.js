81f1e272bc7e421a2d7d5b4acbc1e73d8959ae03
Tony Nguyen <nguyen.tony1015@gmail.com>
2020-12-22 17:03:48 -0500

    adding projects

---
+++ script.js
@@ -0,0 +1,17 @@
+const card = document.getElementById("bio")
+const headshot = document.getElementById("headshot")
+const info = document.querySelectorAll(".seq")
+
+const tl = gsap.timeline({ defaults: { ease: "power2.inOut", duration: 1.5 } })
+
+tl.from(headshot, { xPercent: "-20", opacity: 0 })
+    .from(card, { opacity: 0, duration: .5 }, "-=1")
+    .from(card, { xPercent: "-10", backdropFilter: "blur(0px)" })
+    .from(info, { yPercent: "30", opacity: 0, stagger: .2, duration: .5 }, "-=.5")
+
+
+// const projects = document.getElementById("projects")
+
+// projects.addEventListener("click", () => {
+//     card.classList.toggle("hidden")
+// })
