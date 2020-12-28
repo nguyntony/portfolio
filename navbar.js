971d9b9d5fa0aae8372c177fc07182ba3ed080cd
Tony Nguyen <nguyen.tony1015@gmail.com>
2020-12-28 13:45:02 -0500

    still need to figure out a desktop navbar alongside with fixing the mobile

--- navbar.js
+++ navbar.js
@@ -1,7 +1,11 @@
 const hambuger = document.querySelector(".hamburger")
 const navLinks = document.querySelector(".nav-links")
-const links = document.querySelectorAll(".nav-links li")
+const links = document.querySelectorAll(".nav-links li a")
 
 hambuger.addEventListener("click", () => {
+    navLinks.classList.toggle("open")
+})
+
+links.addEventListener("click", () => {
     navLinks.classList.toggle("open")
 })
