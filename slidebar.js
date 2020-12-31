5ef77cc3bfe639f06b892774db31bdf81a3ab828
Tony Nguyen <nguyen.tony1015@gmail.com>
2020-12-30 20:06:48 -0500

    adding back to top btn

--- slidebar.js
+++ slidebar.js
@@ -1,22 +1,30 @@
-let card = document.getElementById("ham")
-let tl = gsap.timeline({ defaults: { ease: "power2.inOut" } })
+let card = document.getElementById("ham");
+let tl = gsap.timeline({ defaults: { ease: "power2.inOut" } });
 let toggle = false;
 
 tl.to(".ham", {
     background: "#e99685",
-    'borderRadius': "40% 0 0 40%"
+    borderRadius: "40% 0 0 40%",
 });
-tl.to("nav", {
-    "clipPath": "ellipse(100% 100% at 50% 50%)"
-}, "-=.5");
-tl.to("nav a", {
-    opacity: 1,
-    transform: "translateX(0)",
-    stagger: 0.2
-}, "-=.2")
+tl.to(
+    "nav",
+    {
+        clipPath: "ellipse(100% 100% at 50% 50%)",
+    },
+    "-=.5"
+);
+tl.to(
+    "nav a",
+    {
+        opacity: 1,
+        transform: "translateX(0)",
+        stagger: 0.2,
+    },
+    "-=.2"
+);
 tl.pause();
 
 card.addEventListener("click", () => {
     toggle = !toggle;
     if (toggle ? tl.play() : tl.reverse());
-})
+});
